227362205401b9363ecebedef036b29ee2931701
David Chuvik <dchuvik@gmail.com>
2022-05-31 16:00:14 -0400

    variables exercise

--- VariableEx/VariableEx/Program.cs
+++ VariableEx/VariableEx/Program.cs
@@ -1,12 +1,11 @@
 ﻿using System;
 
-namespace Declare_and_initialize_variables
+namespace VariableEx
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-
             string name = "David";
             int age = 22;
             char grade = 'A';
@@ -14,7 +13,9 @@ namespace Declare_and_initialize_variables
             double gpa = 3.94;
             decimal pi = 3.1415926535m;
 
-            Console.WriteLine($"My name is {name} and I am {age} years old. It is {isMarried} that I am married. My college GPA was {gpa} because most of my grades were {grade}'s. The first numbers of pi are {pi}.");
+            Console.WriteLine($"My name is {name} and I am {age} years old. " +
+                $"It is {isMarried} that I am married. My college GPA was {gpa} " +
+                $"because most of my grades were {grade}'s. The first numbers of pi are {pi}.");
         }
     }
 }
